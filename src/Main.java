5c0a3d26638813d8f613fb42febb57bf0b23fc46
hp <govindpurohit@gmail.com>
2022-05-29 22:58:56 +0530

    Object

--- src/Main.java
+++ src/Main.java
@@ -1,16 +1,29 @@
-class Pen{
+class Pen {
     String color;
     String Type;
-    public void write(){
+
+    public void write() {
         System.out.println("Write something");
     }
+
+    public void printcolor() {
+        System.out.println(this.color);
+    }
 }
+
 public class Main {
     public static void main(String[] args) {
-         Pen pen1 = new Pen();
-         pen1.color = "Blue";
-         pen1.Type = "Gel";
+        Pen pen1 = new Pen();
+        pen1.color = "Blue";
+        pen1.Type = "Gel";
 
-         pen1.write();
+        Pen pen2 = new Pen();
+        pen2.color = "Black";
+        pen2.Type = "Ball Point";
+
+        pen1.printcolor();
+        pen2.printcolor();
+        pen2.write();
+        pen1.write();
     }
 }
